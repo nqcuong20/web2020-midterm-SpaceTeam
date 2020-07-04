87bb2c73b1a0ef53c8081bf679b3e7cbe10133f4
Quoc Duy <phamhjuynhquocduy@gmail.com>
2020-07-04 08:40:47 +0700

    lien ket cac table

--- sanpham.sql
+++ sanpham.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 04, 2020 lúc 03:15 AM
+-- Thời gian đã tạo: Th7 04, 2020 lúc 03:37 AM
 -- Phiên bản máy phục vụ: 10.4.11-MariaDB
 -- Phiên bản PHP: 7.4.3
 
@@ -45,7 +45,8 @@ CREATE TABLE `hanghoa` (
   `mahh` int(11) NOT NULL,
   `tenhh` varchar(50) NOT NULL,
   `dongia` int(11) NOT NULL,
-  `hinh` varchar(50) NOT NULL
+  `hinh` varchar(50) NOT NULL,
+  `loai` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -87,20 +88,36 @@ CREATE TABLE `loai` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Đang đổ dữ liệu cho bảng `loai`
+--
+
+INSERT INTO `loai` (`maloai`, `tenloai`, `mota`) VALUES
+(1, '', '');
+
+--
 -- Chỉ mục cho các bảng đã đổ
 --
+
+--
+-- Chỉ mục cho bảng `chitiethoadon`
+--
+ALTER TABLE `chitiethoadon`
+  ADD KEY `mahd` (`mahd`,`mahh`),
+  ADD KEY `mahh` (`mahh`);
 
 --
 -- Chỉ mục cho bảng `hanghoa`
 --
 ALTER TABLE `hanghoa`
-  ADD PRIMARY KEY (`mahh`);
+  ADD PRIMARY KEY (`mahh`),
+  ADD KEY `loai` (`loai`);
 
 --
 -- Chỉ mục cho bảng `hoadon`
 --
 ALTER TABLE `hoadon`
-  ADD PRIMARY KEY (`mahd`);
+  ADD PRIMARY KEY (`mahd`),
+  ADD KEY `makh` (`makh`);
 
 --
 -- Chỉ mục cho bảng `khachhang`
@@ -122,7 +139,7 @@ ALTER TABLE `loai`
 -- AUTO_INCREMENT cho bảng `hanghoa`
 --
 ALTER TABLE `hanghoa`
-  MODIFY `mahh` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `mahh` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `hoadon`
@@ -140,7 +157,30 @@ ALTER TABLE `khachhang`
 -- AUTO_INCREMENT cho bảng `loai`
 --
 ALTER TABLE `loai`
-  MODIFY `maloai` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `maloai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `chitiethoadon`
+--
+ALTER TABLE `chitiethoadon`
+  ADD CONSTRAINT `chitiethoadon_ibfk_1` FOREIGN KEY (`mahd`) REFERENCES `hoadon` (`mahd`),
+  ADD CONSTRAINT `chitiethoadon_ibfk_2` FOREIGN KEY (`mahh`) REFERENCES `hanghoa` (`mahh`);
+
+--
+-- Các ràng buộc cho bảng `hanghoa`
+--
+ALTER TABLE `hanghoa`
+  ADD CONSTRAINT `hanghoa_ibfk_1` FOREIGN KEY (`loai`) REFERENCES `loai` (`maloai`);
+
+--
+-- Các ràng buộc cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD CONSTRAINT `hoadon_ibfk_1` FOREIGN KEY (`makh`) REFERENCES `khachhang` (`makh`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
