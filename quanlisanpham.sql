05df9f83068ebc55e21d2d7914de723417833feb
thiendeptrai29072000 <duongtanthien.com.vn@gmail.com>
2020-07-04 10:48:14 +0700

    quanlisanpham.sql

---
+++ quanlisanpham.sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th7 04, 2020 lúc 05:31 AM
+-- Phiên bản máy phục vụ: 10.4.11-MariaDB
+-- Phiên bản PHP: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `qlsp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `chitiethoadon`
+--
+
+CREATE TABLE `chitiethoadon` (
+  `macthd` int(11) NOT NULL,
+  `mahd` int(11) NOT NULL,
+  `mahh` int(11) NOT NULL,
+  `soluong` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `chitiethoadon`
+--
+
+INSERT INTO `chitiethoadon` (`macthd`, `mahd`, `mahh`, `soluong`) VALUES
+(1, 3, 3, 3),
+(2, 1, 1, 3),
+(3, 1, 1, 2),
+(4, 1, 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hanghoa`
+--
+
+CREATE TABLE `hanghoa` (
+  `mahh` int(11) NOT NULL,
+  `tenhh` varchar(50) NOT NULL,
+  `dongia` int(11) NOT NULL,
+  `hinh` varchar(50) NOT NULL,
+  `loai` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `hanghoa`
+--
+
+INSERT INTO `hanghoa` (`mahh`, `tenhh`, `dongia`, `hinh`, `loai`) VALUES
+(1, 'SAMSUNG GALAXY S20+', 19999000, 'SSGALAXYS20+19TR.png', 3),
+(2, 'SAMSUNG GALAXY FLOD', 50000000, 'SSGALAXYFOLD50TR.png', 3),
+(3, 'SAMSUNG GALAXY  20 ULEA', 2000000, 'SSGALAXYS20ULREA20TR.png', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hoadon`
+--
+
+CREATE TABLE `hoadon` (
+  `mahd` int(11) NOT NULL,
+  `ngaylaphd` date NOT NULL,
+  `makh` int(11) NOT NULL,
+  `tongtien` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `hoadon`
+--
+
+INSERT INTO `hoadon` (`mahd`, `ngaylaphd`, `makh`, `tongtien`) VALUES
+(1, '0000-00-00', 2, 20000),
+(3, '2020-07-08', 1, 10000000),
+(4, '2020-07-01', 2, 2000000);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khachhang`
+--
+
+CREATE TABLE `khachhang` (
+  `makh` int(11) NOT NULL,
+  `hoten` varchar(50) NOT NULL,
+  `dienthoai` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `khachhang`
+--
+
+INSERT INTO `khachhang` (`makh`, `hoten`, `dienthoai`, `email`) VALUES
+(1, 'Nguyễn Quốc Cường', 377077630, 'nguyenquoccuongcn20@gmail.com'),
+(2, 'Nguyễn Quốc Duy', 38592350, 'nguyenquocduy@gmail.com'),
+(3, 'Trần Hoài Đức', 37736235, 'tranhoaiduc@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `loai`
+--
+
+CREATE TABLE `loai` (
+  `maloai` int(11) NOT NULL,
+  `tenloai` varchar(50) NOT NULL,
+  `mota` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `loai`
+--
+
+INSERT INTO `loai` (`maloai`, `tenloai`, `mota`) VALUES
+(1, 'Iphone', 'iphone giá rẻ'),
+(2, 'Nokia', 'Điện Thoại Đẹp'),
+(3, 'Sam Sung', 'Điện Thoại Xinh');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `chitiethoadon`
+--
+ALTER TABLE `chitiethoadon`
+  ADD KEY `mahd` (`mahd`,`mahh`),
+  ADD KEY `mahh` (`mahh`);
+
+--
+-- Chỉ mục cho bảng `hanghoa`
+--
+ALTER TABLE `hanghoa`
+  ADD PRIMARY KEY (`mahh`),
+  ADD KEY `loai` (`loai`);
+
+--
+-- Chỉ mục cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD PRIMARY KEY (`mahd`),
+  ADD KEY `makh` (`makh`);
+
+--
+-- Chỉ mục cho bảng `khachhang`
+--
+ALTER TABLE `khachhang`
+  ADD PRIMARY KEY (`makh`);
+
+--
+-- Chỉ mục cho bảng `loai`
+--
+ALTER TABLE `loai`
+  ADD PRIMARY KEY (`maloai`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `hanghoa`
+--
+ALTER TABLE `hanghoa`
+  MODIFY `mahh` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  MODIFY `mahd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT cho bảng `khachhang`
+--
+ALTER TABLE `khachhang`
+  MODIFY `makh` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `loai`
+--
+ALTER TABLE `loai`
+  MODIFY `maloai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `chitiethoadon`
+--
+ALTER TABLE `chitiethoadon`
+  ADD CONSTRAINT `chitiethoadon_ibfk_1` FOREIGN KEY (`mahd`) REFERENCES `hoadon` (`mahd`),
+  ADD CONSTRAINT `chitiethoadon_ibfk_2` FOREIGN KEY (`mahh`) REFERENCES `hanghoa` (`mahh`);
+
+--
+-- Các ràng buộc cho bảng `hanghoa`
+--
+ALTER TABLE `hanghoa`
+  ADD CONSTRAINT `hanghoa_ibfk_1` FOREIGN KEY (`loai`) REFERENCES `loai` (`maloai`);
+
+--
+-- Các ràng buộc cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD CONSTRAINT `hoadon_ibfk_1` FOREIGN KEY (`makh`) REFERENCES `khachhang` (`makh`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
